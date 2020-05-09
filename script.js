9c09cb82b10eb0e1ed9afc59f5db4de7dc9ed840
PugmanD <59942684+PugmanD@users.noreply.github.com>
2020-05-09 14:24:29 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 var Karambits = 0;
+var audio = document.getElementsById('audio')
 function add(){
   Karambits = Karambits + 1;
   document.getElementsBYId('Karambits').innerHTML = "Karambits: " + Karambits;
+  audio.play();
 }
